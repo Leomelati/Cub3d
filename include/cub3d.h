48338949d514f855510a92dc8a7f463329b3977a
Leonardo Melati <leomelati@gmail.com>
2020-12-21 07:47:57 +0100

    Closing window

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,17 +6,46 @@
 /*   By: lmartins <lmartins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 10:11:40 by lmartins          #+#    #+#             */
-/*   Updated: 2020/12/20 08:00:32 by lmartins         ###   ########.fr       */
+/*   Updated: 2020/12/21 07:31:10 by lmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+/*
+** Includes
+*/
+
 # include "libft.h"
 # include "mlx.h"
 # include <stdio.h>
 # include <math.h>
 
+/*
+** Struct
+*/
+
+typedef struct 	s_parameters
+{
+	void *mlx;
+	void *window;
+	int	width;
+	int height;
+	int floor_color;
+	int ceilling_color;
+	char *north_texture;
+	char *south_texture;
+	char *west_texture;
+	char *east_texture;
+	char *sprite_texture;
+
+}				t_parameters;
+
+/*
+** Defines
+*/
+
+#define ESC 65307
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,17 +6,45 @@
 /*   By: lmartins <lmartins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/13 09:40:01 by lmartins          #+#    #+#             */
-/*   Updated: 2020/09/01 09:04:41 by lmartins         ###   ########.fr       */
+/*   Updated: 2020/12/21 07:41:44 by lmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void	define_resolution(t_parameters *info, char *readed)
+{
+	char	**string;
+
+	if (readed[0] = 'R')
+	{
+		string = ft_split(readed, ' ');
+		info->width = ft_atoi(string[1]);
+		info->height = ft_atoi(string[2]);
+	}
+}
+
+int		key_hook(int keycode, t_parameters *info)
+{
+	printf("%i\n", keycode);
+	if (keycode == ESC)
+	{
+		mlx_destroy_window(info->mlx, info->window);
+		exit(0);
+	}
+}
+
 int		main(int argc, char **argv)
 {
-	void * mlx;
-	mlx = mlx_init();
-	mlx_new_window(mlx, 500, 500, "cub3D");
-	mlx_loop(mlx);
+	char 			*readed;
+	t_parameters	info;
+
+	get_next_line(open(argv[1], O_RDONLY), &readed);
+	define_resolution(&info, readed);
+	info.mlx = mlx_init();
+	info.window = mlx_new_window(info.mlx, info.width, info.height, "cub3D");
+	mlx_key_hook(info.window, key_hook, &info);
+	// mlx_hook(info.window, 2, 1L<<0, key_hook, &info);
+	mlx_loop(info.mlx);
 	return (0);	
 }
