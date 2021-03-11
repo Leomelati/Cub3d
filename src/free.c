09b1df0a6be166ebebc37de493bd0f25cededd03
Leonardo Melati <leomelati@gmail.com>
2021-03-11 03:19:12 +0100

    Fixed free

--- src/free.c
+++ src/free.c
@@ -6,11 +6,47 @@
 /*   By: lmartins <lmartins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/01 22:45:11 by lmartins          #+#    #+#             */
-/*   Updated: 2021/03/10 02:51:54 by lmartins         ###   ########.fr       */
+/*   Updated: 2021/03/11 03:16:56 by lmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	ft_free_map(t_parameters *info)
+{
+	int		i;
+	
+	i = 0;
+	while (i < info->map->map_y && info->map->map[i])
+	{
+		free(info->map->map[i]);
+		info->map->map[i] = NULL;
+		i++;
+	}
+	if (info->map->map)
+	{
+		free(info->map->map);
+		info->map->map = NULL;
+	}
+}
+
+void	ft_free_rays(t_parameters *info)
+{
+	int		i;
+
+	i = 0;
+	while (i <= info->map->num_rays)
+	{
+		if (info->ray[i])
+		{
+			free(info->ray[i]);
+			info->ray[i] = NULL;
+		}
+		i++;
+	}
+	free(info->ray);
+	info->ray = NULL;
+}
 
 void	ft_free_img(t_parameters *info, t_img *img)
 {
@@ -26,13 +62,11 @@ int		destroy_window(t_parameters *info)
 {
 	int		i;
 
+	ft_free_rays(info);
+	ft_free_map(info);
 	ft_free_img(info, info->img);
 	if (info->win)
 		mlx_destroy_window(info->mlx, info->win);
-	if (info->map)
-		free(info->map);
-	if (info->ray)
-		free(info->ray);
 	free(info->mlx);
 	exit(0);
 	return (0);
