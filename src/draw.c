431984048488ca30cc11d1bd9b954c47b12a5704
Leonardo Melati <leomelati@gmail.com>
2021-03-11 01:32:36 +0100

    Adding textures

--- src/draw.c
+++ src/draw.c
@@ -6,30 +6,37 @@
 /*   By: lmartins <lmartins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/07 04:39:43 by lmartins          #+#    #+#             */
-/*   Updated: 2021/03/10 05:34:08 by lmartins         ###   ########.fr       */
+/*   Updated: 2021/03/11 01:28:37 by lmartins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	draw_3dwall(float wall_proj_height, int id, t_img *img,
-	t_parameters *info)
-{
-	t_coordinates	start;
-	t_coordinates	end;
+// void	draw_3dwall(float wall_proj_height, int id, t_img *img,
+// 	t_parameters *info)
+// {
+// 	t_coordinates	start;
+// 	t_coordinates	end;
 
-	start.x = id + 1;
-	start.y = (info->width / 2) - (wall_proj_height / 2);
-	start.y = (start.y > info->height * TILE_SIZE) ? info->height : start.y;
-	start.y = (start.y < 0) ? 0 : start.y;
-	end.x = id + 1;
-	end.y = start.y + wall_proj_height;
-	end.y = (end.y > info->height * TILE_SIZE) ? info->height : end.y;
-	end.y = (end.y < 0) ? 0 : end.y;
-	if (info->ray[id]->vertical_hit)
-		ft_draw_line(img, start, end, 0x00CA5F64);
-	else
-		ft_draw_line(img, start, end, 0x00ACF546);
+// 	start.x = id + 1;
+// 	start.y = (info->width / 2) - (wall_proj_height / 2);
+// 	start.y = (start.y > info->height * TILE_SIZE) ? info->height : start.y;
+// 	start.y = (start.y < 0) ? 0 : start.y;
+// 	end.x = id + 1;
+// 	end.y = start.y + wall_proj_height;
+// 	end.y = (end.y > info->height * TILE_SIZE) ? info->height : end.y;
+// 	end.y = (end.y < 0) ? 0 : end.y;
+// 	if (info->ray[id]->vertical_hit)
+// 		ft_draw_line(img, start, end, 0x00CA5F64);
+// 	else
+// 		ft_draw_line(img, start, end, 0x00ACF546);
+// }
+
+int		get_color(t_parameters *info, int column_id)
+{
+	if (info->ray[column_id]->vertical_hit)
+		return (0x00CA5F64);
+	return (0x00ACF546);
 }
 
 void	wall_limits(t_img *img, t_parameters *info,
@@ -41,16 +48,20 @@ void	wall_limits(t_img *img, t_parameters *info,
 
 	top_pixel = (info->height / 2) - (wall_height / 2);
 	top_pixel = (top_pixel < 0) ? 0 : top_pixel;
+	bottom_pixel = (info->height / 2) + (wall_height / 2);
+	bottom_pixel = (bottom_pixel > info->height) ? info->height : bottom_pixel;
 	i = 0;
-	while (i < top_pixel)
+	while (i <= top_pixel)
 	{
 		ft_pixel_put(img, column_id, i, info->ceilling_color);
 		i++;
 	}
-	bottom_pixel = (info->height / 2) + (wall_height / 2);
-	bottom_pixel = (bottom_pixel > info->height) ? info->height : bottom_pixel;
-	i = bottom_pixel;
-	while (i < info->height)
+	while (i <= bottom_pixel)
+	{
+		ft_pixel_put(img, column_id, i, get_color(info, column_id));
+		i++;
+	}
+	while (i <= info->height)
 	{
 		ft_pixel_put(img, column_id, i, info->floor_color);
 		i++;
